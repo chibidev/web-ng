40e3e253024bd1eb54aa74a2a4cc9b79f4508676
chibidev <zsolt.erhardt@gmail.com>
2012-02-07 22:59:03 +0100

    Associated sockets with streams, added gitignore file

--- http-test/src/main.cpp
+++ http-test/src/main.cpp
@@ -5,7 +5,8 @@
 int main() {
 	using namespace std;
 
-	inet4stream ns("127.0.0.1", 80);
+	std::socket<inet4_address> sock(inet4_address("127.0.0.1", 80));
+	inet4stream ns(sock);
 	ns << "GET / HTTP/1.1\r\n" << "Host: localhost\r\n" << "\r\n";
 	ns.flush();
 	while (ns.good()) {
